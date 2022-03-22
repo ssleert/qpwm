64015a7bc98e1075a2b10c1fedf22275aa197ba7
sfome <68077937+ssleert@users.noreply.github.com>
2022-03-22 22:36:04 +0300

    Add files via upload

--- qpwm.c
+++ qpwm.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "qpwm.h"
 
@@ -137,8 +138,7 @@ void win_fs(const Arg arg) {
 
     if ((cur->f = cur->f ? 0 : 1)) {
         win_size(cur->w, &cur->wx, &cur->wy, &cur->ww, &cur->wh);
-        XMoveResizeWindow(d, cur->w, 0, 0, sw, sh);
-
+        XMoveResizeWindow(d, cur->w, 0, GAP_SIZE, sw, sh - GAP_SIZE);
     } else {
         XMoveResizeWindow(d, cur->w, cur->wx, cur->wy, cur->ww, cur->wh);
     }
@@ -187,7 +187,17 @@ void ws_go(const Arg arg) {
 
     ws_sel(tmp);
 
-    for win XUnmapWindow(d, c->w);
+    for win {
+		char* winame = NULL;
+		if (!XFetchName(d, c->w, &winame) || winame == NULL) {
+			XUnmapWindow(d, c->w);
+		} else {
+			if (strncmp(winame, barname, strlen(barname))) {
+				XUnmapWindow(d, c->w);
+			}
+			XFree(winame);
+		}
+    }
 
     ws_sel(arg.i);
 
@@ -278,7 +288,7 @@ int main(void) {
     root  = RootWindow(d, s);
     sw    = XDisplayWidth(d, s);
     sh    = XDisplayHeight(d, s);
-
+         
     XSelectInput(d,  root, SubstructureRedirectMask);
     XDefineCursor(d, root, XCreateFontCursor(d, 68));
     input_grab(root);
